ae827af9ea0e3e15ed2c8149bc2a41afa4a819ab
Stanleyowskiki <95365847+Stanleyowskiki@users.noreply.github.com>
2021-12-12 16:38:14 +0100

    New update

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -5,18 +5,17 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-	int n = sizeof(b)/sizeof(b[0])
-	int m = sizeof(x)/sizeof(x[0])
-	int h = sizeof(mat)/sizeof(mat[0])
+	double s;
+	int w, k;
 	if (n != m || m != h*h || n != h*h)
 		return 2;
- 	for( int w= n-1; w >= 0; w-- ) {
-		double s = 0;
-		for( int k= w+1; k < n; k++ )
-			s += mat[w*n+k]*x[k];
-		if( mat[w*n+w] == 0)
+ 	for( w= n-1; w >= 0; w-- ) {
+		s = 0;
+		for( k= w+1; k < n; k++ )
+			s += mat->data[w][k]*x->data[k][0];
+		if( mat->data[w][w] == 0)
 			return 1;
-		x[w] = ( b[w] - s ) / mat[w*n+w];
+		x->data[w][0] = ( b->data[w][0] - s ) / mat->data[w][w];
 	}
 	return 0;
 }
