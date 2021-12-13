0513f8d80058d35cd666df1c48ea7fec3883a581
Weronika Zbierowska <hello.weri@vp.pl>
2021-12-13 20:00:32 +0100

    Wybor elementu dominujacego v1

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -6,11 +6,29 @@
  */
 int eliminate(Matrix *mat, Matrix *b)
 {
-	int i, j, k;
+	int i, j, k, max;
 	double q;
-	
+	double *tmp;
+
 	for(i = 0; i < (mat->c - 1); i++)
 	{
+        max = i;
+        for(j = i+1; j < (mat->r); j++)
+        {
+            if(mat->data[j][i] > mat->data[max][i])
+            {
+                max = j;
+            }
+        }
+        if(max != i)
+        {
+            tmp = mat->data[i];
+            mat->data[i] = mat->data[max];
+            mat->data[max] = tmp;
+            tmp = b->data[i];
+            b->data[i] = b->data[max];
+            b->data[max] = tmp;
+        }
 		for(j = i+1; j < (mat->r); j++)
 		{
 			if(mat->data[i][i] != 0)
